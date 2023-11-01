0843ffc612216ea54acd2591099a6c7d9a9af78c
Matt Rutkowski <mrutkows@us.ibm.com>
2023-11-01 17:08:02 -0500

    Complete converting CycloneDX common/reused structs to use pointers
    Signed-off-by: Matt Rutkowski <mrutkows@us.ibm.com>

--- schema/cyclonedx_common.go
+++ schema/cyclonedx_common.go
@@ -82,26 +82,26 @@ type CDXNameDescription struct {
 // v1.4: created "note" defn.
 // Note: "locale" is of type "localeType" which is a constrained `string`
 type CDXNote struct {
-	Locale string        `json:"locale,omitempty"`
-	Text   CDXAttachment `json:"attachment,omitempty"`
+	Locale string         `json:"locale,omitempty"`
+	Text   *CDXAttachment `json:"attachment,omitempty"`
 }
 
 // v1.2: existed
 // v1.5: added "bom-ref"
 type CDXOrganizationalEntity struct {
-	Name    string                     `json:"name,omitempty"`
-	Url     []string                   `json:"url,omitempty"`
-	Contact []CDXOrganizationalContact `json:"contact,omitempty"`
-	BOMRef  CDXRefType                 `json:"bom-ref,omitempty"` // v1.5 added
+	Name    string                      `json:"name,omitempty"`
+	Url     []string                    `json:"url,omitempty"`
+	Contact *[]CDXOrganizationalContact `json:"contact,omitempty"`
+	BOMRef  *CDXRefType                 `json:"bom-ref,omitempty"` // v1.5 added
 }
 
 // v1.2: existed
 // v1.5: added "bom-ref"
 type CDXOrganizationalContact struct {
-	Name   string     `json:"name,omitempty"`
-	Email  string     `json:"email,omitempty"`
-	Phone  string     `json:"phone,omitempty"`
-	BOMRef CDXRefType `json:"bom-ref,omitempty"` // v1.5 added
+	Name   string      `json:"name,omitempty"`
+	Email  string      `json:"email,omitempty"`
+	Phone  string      `json:"phone,omitempty"`
+	BOMRef *CDXRefType `json:"bom-ref,omitempty"` // v1.5 added
 }
 
 // v1.3: created "property" defn.
@@ -113,8 +113,8 @@ type CDXProperty struct {
 // v1.5: created for reuse in "licensing" schema for "licensee" and "licensor"
 // TODO: reuse on "annotator" as well?
 type CDXLicenseLegalParty struct {
-	Organization CDXOrganizationalEntity  `json:"organization,omitempty"`
-	Individual   CDXOrganizationalContact `json:"individual,omitempty"`
+	Organization *CDXOrganizationalEntity  `json:"organization,omitempty"`
+	Individual   *CDXOrganizationalContact `json:"individual,omitempty"`
 }
 
 // v1.2: existed
@@ -124,15 +124,15 @@ type CDXLicenseLegalParty struct {
 // - v1.5: In order to support the new object "Creation Tools", we need to combine these fields
 // into with the legacy structure fields
 type CDXLegacyCreationTool struct {
-	Vendor             string                 `json:"vendor,omitempty" cdx:"deprecated"`       // v1.5: deprecated
-	Name               string                 `json:"name,omitempty" cdx:"deprecated"`         // v1.5: deprecated
-	Version            string                 `json:"version,omitempty" cdx:"deprecated"`      // v1.5: deprecated
-	Hashes             []CDXHash              `json:"hashes,omitempty" cdx:"deprecated"`       // v1.5: deprecated
-	ExternalReferences []CDXExternalReference `json:"externalReferences,omitempty" cdx:"+1.4"` // v1.4: added, v1.5: deprecated
+	Vendor             string                  `json:"vendor,omitempty" cdx:"deprecated"`       // v1.5: deprecated
+	Name               string                  `json:"name,omitempty" cdx:"deprecated"`         // v1.5: deprecated
+	Version            string                  `json:"version,omitempty" cdx:"deprecated"`      // v1.5: deprecated
+	Hashes             *[]CDXHash              `json:"hashes,omitempty" cdx:"deprecated"`       // v1.5: deprecated
+	ExternalReferences *[]CDXExternalReference `json:"externalReferences,omitempty" cdx:"+1.4"` // v1.4: added, v1.5: deprecated
 }
 
 // v1.5: created. Intended to be used instead of (legacy) Creation Tools which was deprecated
 type CDXCreationTools struct {
-	Components []CDXComponent `json:"components,omitempty" cdx:"+1.5"` // v1.5: added (new type)
-	Services   []CDXService   `json:"services,omitempty" cdx:"+1.5"`   // v1.5: added (new type)
+	Components *[]CDXComponent `json:"components,omitempty" cdx:"+1.5"` // v1.5: added (new type)
+	Services   *[]CDXService   `json:"services,omitempty" cdx:"+1.5"`   // v1.5: added (new type)
 }
